49db4f64378c11dbdc478d4e3046c9764e302383
yogieputra8 <yogie.putra8@gmail.com>
2019-03-04 20:15:47 +0700

    add function

---
+++ function/SampleOne.js
@@ -0,0 +1,20 @@
+// 1. Keyword
+// 2. Nama function
+
+// // Konvensional
+// function penjumlahan(a, b){
+//   const hasil = a + b
+//   return hasil
+// }
+
+// ES6 - Fat arrow
+const penjumlahan = (a, b) => {
+  const hasil = a + b
+  return hasil
+}
+
+const hasilPenjumlahan = penjumlahan(5, 3)
+console.log(hasilPenjumlahan)
+
+
+
